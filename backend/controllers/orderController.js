63fe377b2eef93481726f17ff2df7592603bbc38
savyasanchi shukla <you@example.com>
2022-12-07 19:48:54 +0530

    Final Commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -42,7 +42,6 @@ export const addOrderItems = asyncHandler(async (req, res) => {
 export const getOrderById = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id).populate("user", ["name", "email"]);
   if (order) {
-    // console.log(order)
     res.json(order);
   } else {
     res.status(404);
